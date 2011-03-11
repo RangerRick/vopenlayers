1a3ca8a1f72dba1e02d76201f581fc51722cca79
matti.tahvonen <matti.tahvonen@37e336fd-f6aa-ac36-3442-120c8ea9bd2d>
2011-03-11 19:08:10 +0000

    forgotten class from last commit
    git-svn-id: http://vopenlayers.googlecode.com/svn/trunk@14 37e336fd-f6aa-ac36-3442-120c8ea9bd2d

---
+++ src/main/java/org/vaadin/vol/Bounds.java
@@ -0,0 +1,35 @@
+package org.vaadin.vol;
+
+public class Bounds {
+	
+	private double top;
+	private double bottom;
+	private double left;
+	private double right;
+	
+	public void setTop(double top) {
+		this.top = top;
+	}
+	public double getTop() {
+		return top;
+	}
+	public void setBottom(double bottom) {
+		this.bottom = bottom;
+	}
+	public double getBottom() {
+		return bottom;
+	}
+	public void setLeft(double left) {
+		this.left = left;
+	}
+	public double getLeft() {
+		return left;
+	}
+	public void setRight(double right) {
+		this.right = right;
+	}
+	public double getRight() {
+		return right;
+	}
+
+}
