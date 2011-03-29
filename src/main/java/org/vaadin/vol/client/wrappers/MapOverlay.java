6e20b88a2b804e63a213ac6b2053c8905f1cbfaa
matti.tahvonen <matti.tahvonen@37e336fd-f6aa-ac36-3442-120c8ea9bd2d>
2011-03-29 18:01:37 +0000

    inject "firebug lite" for old browsers to GWT frame. OL does this for main window, but exceptions happen in some cases as some of the code is executed in GWT frames context.
    git-svn-id: http://vopenlayers.googlecode.com/svn/trunk@20 37e336fd-f6aa-ac36-3442-120c8ea9bd2d

--- src/main/java/org/vaadin/vol/client/wrappers/MapOverlay.java
+++ src/main/java/org/vaadin/vol/client/wrappers/MapOverlay.java
@@ -27,6 +27,21 @@ public class MapOverlay extends AbstractOpenLayersWrapper {
 	//	                maxResolution: 156543.0339,
 	//	                maxExtent: new $wnd.OpenLayers.Bounds(-20037508, -20037508, 20037508, 20037508.34)
 		            };
+		
+		 //
+		 // Rude hack to add "firebug lite" to GWT iframe (actually to all iframes). 
+		 // Without this some OL functions may fail without modern browser or firebug.
+		 //
+		var iframes = $doc.getElementsByTagName("iframe");
+		for(var i = 0; i < iframes.length; i++) {
+			var frame = iframes[i].contentWindow;
+			try {
+				if(!frame.console) {
+					var e = function(){};
+					frame.console = {log:e,error:e,dir:e,debug:e,info:e,warn:e,error:e,userError:e,assert:e,dirXml:e}
+				}
+			} catch(e){};
+		}
 
 		return new $wnd.OpenLayers.Map(id, options);
 	}-*/;
