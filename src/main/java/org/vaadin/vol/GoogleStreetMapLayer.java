056cdc8c2bac9cbd83ab5f4a62a07e947cb1e7f1
matti.tahvonen <matti.tahvonen@37e336fd-f6aa-ac36-3442-120c8ea9bd2d>
2011-03-30 09:48:40 +0000

    enhanced javadocs
    git-svn-id: http://vopenlayers.googlecode.com/svn/trunk@22 37e336fd-f6aa-ac36-3442-120c8ea9bd2d

--- src/main/java/org/vaadin/vol/GoogleStreetMapLayer.java
+++ src/main/java/org/vaadin/vol/GoogleStreetMapLayer.java
@@ -8,6 +8,17 @@ import org.vaadin.vol.client.ui.VGoogleStreetMapLayer;
 import com.vaadin.ui.AbstractComponent;
 import com.vaadin.ui.ClientWidget;
 
+/**
+ * Google street layer that can be added to {@link OpenLayersMap}.
+ * <p>
+ * <strong>Note</strong> that to use this layer, developer must ensure the host
+ * page has google map javascripts inlcuded. This can be achived for example by adding a following script tag to widgetset (...gwt.xml).
+ * 
+ * <p>
+ * <code>
+ *  &lt;script src="http://maps.google.com/maps/api/js?v=3.2&amp;sensor=false"&gt;&lt;/script&gt;
+ * </code>
+ */
 @ClientWidget(VGoogleStreetMapLayer.class)
 public class GoogleStreetMapLayer extends AbstractComponent implements Layer {
 }
--- src/main/java/org/vaadin/vol/OpenStreetMapLayer.java
+++ src/main/java/org/vaadin/vol/OpenStreetMapLayer.java
@@ -5,11 +5,20 @@ package org.vaadin.vol;
 
 import org.vaadin.vol.client.ui.VOpenStreetMapLayer;
 
-import com.vaadin.terminal.PaintException;
-import com.vaadin.terminal.PaintTarget;
 import com.vaadin.ui.AbstractComponent;
 import com.vaadin.ui.ClientWidget;
 
+/**
+ * OpenStreeMap layer that can be added to {@link OpenLayersMap}.
+ * <p>
+ * <strong>Note</strong> that to use this layer, developer must ensure the host
+ * page has open street map javascripts inlcuded. This can be achived for example by adding a following script tag to widgetset (...gwt.xml).
+ * 
+ * <p>
+ * <code>
+ *  &lt;script src="http://www.openstreetmap.org/openlayers/OpenStreetMap.js"&gt;&lt;/script&gt;
+ * </code>
+ */
 @ClientWidget(VOpenStreetMapLayer.class)
 public class OpenStreetMapLayer extends AbstractComponent implements Layer {
 }
