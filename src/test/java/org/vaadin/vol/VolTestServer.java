0a3fc75a51e31bc5ddf33218ab7bedc3eb131dbb
matti.tahvonen@gmail.com <matti.tahvonen@gmail.com@37e336fd-f6aa-ac36-3442-120c8ea9bd2d>
2011-07-11 13:17:40 +0000

    gwt:compile now works for testing. Compiles to target/testwebapp where also VolTestServer (embedded jetty) looks for widgetsets
    git-svn-id: http://vopenlayers.googlecode.com/svn/trunk@55 37e336fd-f6aa-ac36-3442-120c8ea9bd2d

--- src/test/java/org/vaadin/vol/VolTestServer.java
+++ src/test/java/org/vaadin/vol/VolTestServer.java
@@ -5,44 +5,49 @@ import java.io.File;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.vaadin.vol.demo.VolApplication;
+
+import com.vaadin.terminal.gwt.server.ApplicationServlet;
 
 public class VolTestServer {
 
-	private static final int PORT = 9998;
+    private static final int PORT = 9998;
 
-	/**
-	 * 
-	 * Test server for the addon that does not fuck with you like m2eclipse and
-	 * its WTP integration.
-	 * 
-	 * @param args
-	 * @throws Exception
-	 */
-	public static void main(String[] args) throws Exception {
-		Server server = new Server();
+    /**
+     * 
+     * Test server for the addon that does not fuck with you like m2eclipse and
+     * its WTP integration.
+     * 
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        Server server = new Server();
 
-		final Connector connector = new SelectChannelConnector();
+        final Connector connector = new SelectChannelConnector();
 
-		connector.setPort(PORT);
-		server.setConnectors(new Connector[] { connector });
+        connector.setPort(PORT);
+        server.setConnectors(new Connector[] { connector });
 
-		WebAppContext context = new WebAppContext();
-		context.setDescriptor("./src/main/webapp/WEB-INF/web.xml");
-		File file = new File("./target");
-		File[] listFiles = file.listFiles();
-		for (File file2 : listFiles) {
-			if (file2.isDirectory() && file2.getName().startsWith("vol")) {
-				context.setWar(file2.getPath());
-				break;
-			}
-		}
-		context.setContextPath("/");
+        WebAppContext context = new WebAppContext();
+        ServletHolder servletHolder = new ServletHolder(
+                ApplicationServlet.class);
+        servletHolder.setInitParameter("widgetset",
+                "org.vaadin.vol.demo.VolExampleAppWidgetset");
+        servletHolder.setInitParameter("application",
+                VolApplication.class.getName());
 
-		server.setHandler(context);
+        File file = new File("./target/testwebapp");
+        context.setWar(file.getPath());
+        context.setContextPath("/");
+        context.addServlet(servletHolder, "/*");
 
-		server.start();
-		server.join();
-	}
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+    }
 
 }
