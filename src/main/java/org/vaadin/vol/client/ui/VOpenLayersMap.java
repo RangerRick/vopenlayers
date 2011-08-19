daddceb02e32138293d49041477d995af330af89
matti.tahvonen@gmail.com <matti.tahvonen@gmail.com@37e336fd-f6aa-ac36-3442-120c8ea9bd2d>
2011-08-19 08:15:28 +0000

    control updates are now down before layer updates. Fixes some situations where drawing mode couldn't be initialized.
    git-svn-id: http://vopenlayers.googlecode.com/svn/trunk@60 37e336fd-f6aa-ac36-3442-120c8ea9bd2d

--- src/main/java/org/vaadin/vol/client/ui/VOpenLayersMap.java
+++ src/main/java/org/vaadin/vol/client/ui/VOpenLayersMap.java
@@ -28,247 +28,242 @@ import com.vaadin.terminal.gwt.client.VConsole;
  */
 public class VOpenLayersMap extends FlowPanel implements Container {
 
-	/** Set the CSS class name to allow styling. */
-	public static final String CLASSNAME = "v-openlayersmap";
+    /** Set the CSS class name to allow styling. */
+    public static final String CLASSNAME = "v-openlayersmap";
 
-	private static final Projection DEFAULT_PROJECTION = Projection
-			.get("EPSG:4326");
+    private static final Projection DEFAULT_PROJECTION = Projection
+            .get("EPSG:4326");
 
-	/** The client side widget identifier */
-	protected String paintableId;
+    /** The client side widget identifier */
+    protected String paintableId;
 
-	/** Reference to the server connection object. */
-	protected ApplicationConnection client;
+    /** Reference to the server connection object. */
+    protected ApplicationConnection client;
 
-	// private MarkerLayer markerLayer;
+    // private MarkerLayer markerLayer;
 
-	HashMap<String, Widget> components = new HashMap<String, Widget>();
+    HashMap<String, Widget> components = new HashMap<String, Widget>();
 
-	private Map map = new Map();
+    private Map map = new Map();
 
-	FlowPanel fakePaintables = new FlowPanel();
+    FlowPanel fakePaintables = new FlowPanel();
 
-	private HashSet<String> orphanedcomponents;
+    private HashSet<String> orphanedcomponents;
 
-	private GwtOlHandler extentChangeListener;
+    private GwtOlHandler extentChangeListener;
 
-	private boolean immediate;
+    private boolean immediate;
 
-	private HashMap<String,Control> myControls = new HashMap<String,Control>();
+    private HashMap<String, Control> myControls = new HashMap<String, Control>();
 
-	/**
-	 * The constructor should first call super() to initialize the component and
-	 * then handle any initialization relevant to Vaadin.
-	 */
-	public VOpenLayersMap() {
-		myControls.put("PanZoom", null);
-		setWidth("500px");
-		setHeight("500px");
-		add(map);
-		add(fakePaintables);
-		fakePaintables.setVisible(false);
+    /**
+     * The constructor should first call super() to initialize the component and
+     * then handle any initialization relevant to Vaadin.
+     */
+    public VOpenLayersMap() {
+        setWidth("500px");
+        setHeight("500px");
+        add(map);
+        add(fakePaintables);
+        fakePaintables.setVisible(false);
 
-		// This method call of the Paintable interface sets the component
-		// style name in DOM tree
-		setStyleName(CLASSNAME);
-	}
+        // This method call of the Paintable interface sets the component
+        // style name in DOM tree
+        setStyleName(CLASSNAME);
+    }
 
-	/**
-	 * Called whenever an update is received from the server
-	 */
-	public void updateFromUIDL(UIDL uidl, final ApplicationConnection client) {
+    /**
+     * Called whenever an update is received from the server
+     */
+    public void updateFromUIDL(UIDL uidl, final ApplicationConnection client) {
 
-		// This call should be made first.
-		// It handles sizes, captions, tooltips, etc. automatically.
-		if (client.updateComponent(this, uidl, true)) {
-			// If client.updateComponent returns true there has been no changes
-			// and we
-			// do not need to update anything.
-			return;
-		}
+        // This call should be made first.
+        // It handles sizes, captions, tooltips, etc. automatically.
+        if (client.updateComponent(this, uidl, true)) {
+            // If client.updateComponent returns true there has been no changes
+            // and we
+            // do not need to update anything.
+            return;
+        }
 
-		immediate = uidl.hasAttribute("immediate");
+        immediate = uidl.hasAttribute("immediate");
 
-		if (uidl.hasAttribute("jsMapOptions")) {
-			map.setMapInitOptions(uidl.getStringAttribute("jsMapOptions"));
-		}
+        if (uidl.hasAttribute("jsMapOptions")) {
+            map.setMapInitOptions(uidl.getStringAttribute("jsMapOptions"));
+        }
 
-		if (extentChangeListener == null) {
-			extentChangeListener = new GwtOlHandler() {
-				public void onEvent(JsArray arguments) {
-					int zoom = map.getZoom();
-					client.updateVariable(paintableId, "zoom", zoom, false);
-					Bounds extent = map.getExtent();
-					if (extent == null) {
-						VConsole.log(" extent null");
-						return;
-					}
-					Projection projection = map.getProjection();
-					extent.transform(projection, DEFAULT_PROJECTION);
-					client.updateVariable(paintableId, "left",
-							extent.getLeft(), false);
-					client.updateVariable(paintableId, "right",
-							extent.getRight(), false);
-					client.updateVariable(paintableId, "top", extent.getTop(),
-							false);
-					client.updateVariable(paintableId, "bottom",
-							extent.getBottom(), immediate);
-				}
-			};
-			getMap().registerEventHandler("moveend", extentChangeListener);
-			getMap().registerEventHandler("zoomed", extentChangeListener);
+        if (extentChangeListener == null) {
+            extentChangeListener = new GwtOlHandler() {
+                public void onEvent(JsArray arguments) {
+                    int zoom = map.getZoom();
+                    client.updateVariable(paintableId, "zoom", zoom, false);
+                    Bounds extent = map.getExtent();
+                    if (extent == null) {
+                        VConsole.log(" extent null");
+                        return;
+                    }
+                    Projection projection = map.getProjection();
+                    extent.transform(projection, DEFAULT_PROJECTION);
+                    client.updateVariable(paintableId, "left",
+                            extent.getLeft(), false);
+                    client.updateVariable(paintableId, "right",
+                            extent.getRight(), false);
+                    client.updateVariable(paintableId, "top", extent.getTop(),
+                            false);
+                    client.updateVariable(paintableId, "bottom",
+                            extent.getBottom(), immediate);
+                }
+            };
+            getMap().registerEventHandler("moveend", extentChangeListener);
+            getMap().registerEventHandler("zoomed", extentChangeListener);
 
-			/*
-			 * Update extent on first paint.
-			 */
-			// extentChangeListener.onEvent(null);
-		}
+            /*
+             * Update extent on first paint.
+             */
+            // extentChangeListener.onEvent(null);
+        }
 
-		// Save reference to server connection object to be able to send
-		// user interaction later
-		this.client = client;
+        // Save reference to server connection object to be able to send
+        // user interaction later
+        this.client = client;
 
-		// Save the client side identifier (paintable id) for the widget
-		paintableId = uidl.getId();
+        // Save the client side identifier (paintable id) for the widget
+        paintableId = uidl.getId();
 
-		if (uidl.getBooleanAttribute("componentsPainted")) {
-			orphanedcomponents = new HashSet<String>(components.keySet());
+        updateControls(uidl);
 
-			Iterator<Object> childIterator = uidl.getChildIterator();
-			while (childIterator.hasNext()) {
-				UIDL layerUidl = (UIDL) childIterator.next();
-				orphanedcomponents.remove(layerUidl.getId());
-				Paintable paintable = client.getPaintable(layerUidl);
-				if (!components.containsKey(layerUidl.getId())) {
-					components.put(layerUidl.getId(), (Widget) paintable);
-					fakePaintables.add((Widget) paintable);
-				}
-				paintable.updateFromUIDL(layerUidl, client);
+        if (uidl.getBooleanAttribute("componentsPainted")) {
+            orphanedcomponents = new HashSet<String>(components.keySet());
 
-			}
-		}
+            Iterator<Object> childIterator = uidl.getChildIterator();
+            while (childIterator.hasNext()) {
+                UIDL layerUidl = (UIDL) childIterator.next();
+                orphanedcomponents.remove(layerUidl.getId());
+                Paintable paintable = client.getPaintable(layerUidl);
+                if (!components.containsKey(layerUidl.getId())) {
+                    components.put(layerUidl.getId(), (Widget) paintable);
+                    fakePaintables.add((Widget) paintable);
+                }
+                paintable.updateFromUIDL(layerUidl, client);
 
-		if (uidl.hasAttribute("re_top")) {
-			Bounds bounds = Bounds.create(uidl.getDoubleAttribute("re_left"),
-					uidl.getDoubleAttribute("re_bottom"),
-					uidl.getDoubleAttribute("re_right"),
-					uidl.getDoubleAttribute("re_top"));
-			bounds.transform(DEFAULT_PROJECTION, getMap().getProjection());
-			map.setRestrictedExtent(bounds);
-		}
+            }
+        }
 
-		updateZoomAndCenter(uidl);
+        if (uidl.hasAttribute("re_top")) {
+            Bounds bounds = Bounds.create(uidl.getDoubleAttribute("re_left"),
+                    uidl.getDoubleAttribute("re_bottom"),
+                    uidl.getDoubleAttribute("re_right"),
+                    uidl.getDoubleAttribute("re_top"));
+            bounds.transform(DEFAULT_PROJECTION, getMap().getProjection());
+            map.setRestrictedExtent(bounds);
+        }
 
-		if (uidl.hasAttribute("controls")) {
-			
-			HashSet<String> oldcontrols = new HashSet<String>(myControls.keySet());
-			
-			String[] controls = uidl.getStringArrayAttribute("controls");
-			for (int i = 0; i < controls.length; i++) {
-				String name = controls[i];
-				if(oldcontrols.contains(name)) {
-					oldcontrols.remove(name);
-				} else {
-					Control controlByName = Control.getControlByName(name, getMap());
-					if(controlByName != null) {
-						map.addControl(controlByName);
-						myControls.put(name, controlByName);
-					} else {
-						VConsole.error("Control not in OL build: " + name);
-					}
-				}
-			}
-			
-			for (String string : oldcontrols) {
-				Control control = myControls.get(string);
-				if(control != null) {
-					map.removeControl(control);
-				} else {
-					// default control not in myControls
-					JsArray<Control> controls2 = map.getControls();
-					for(int i = 0; i < controls2.length(); i++) {
-						Control control2 = controls2.get(i);
-						if(control2.getId().contains("." + string + "_")) {
-							map.removeControl(control2);
-							break;
-						}
-					}
-				}
-				myControls.remove(string);
-			}
+        updateZoomAndCenter(uidl);
 
-		}
+        if (uidl.getBooleanAttribute("componentsPainted")) {
+            for (String id : orphanedcomponents) {
+                Widget remove = components.remove(id);
+                fakePaintables.remove(remove);
+            }
+        }
 
-		if (uidl.getBooleanAttribute("componentsPainted")) {
-			for (String id : orphanedcomponents) {
-				Widget remove = components.remove(id);
-				fakePaintables.remove(remove);
-			}
-		}
+    }
 
-	}
+    private void updateControls(UIDL uidl) {
+        if (uidl.hasAttribute("controls")) {
 
-	private void updateZoomAndCenter(UIDL uidl) {
+            HashSet<String> oldcontrols = new HashSet<String>(
+                    myControls.keySet());
 
-		if (uidl.hasAttribute("ze_top")) {
-			/*
-			 * Zoom to extent
-			 */
-			double top = uidl.getDoubleAttribute("ze_top");
-			double right = uidl.getDoubleAttribute("ze_right");
-			double bottom = uidl.getDoubleAttribute("ze_bottom");
-			double left = uidl.getDoubleAttribute("ze_left");
-			Bounds bounds = Bounds.create(left, bottom, right, top);
-			bounds.transform(DEFAULT_PROJECTION, getMap().getProjection());
-			getMap().zoomToExtent(bounds);
-			return;
-		}
+            String[] controls = uidl.getStringArrayAttribute("controls");
+            for (int i = 0; i < controls.length; i++) {
+                String name = controls[i];
+                if (oldcontrols.contains(name)) {
+                    oldcontrols.remove(name);
+                } else {
+                    Control controlByName = Control.getControlByName(name,
+                            getMap());
+                    if (controlByName != null) {
+                        map.addControl(controlByName);
+                        myControls.put(name, controlByName);
+                    } else {
+                        VConsole.error("Control not in OL build: " + name);
+                    }
+                }
+            }
 
-		int zoom = map.getZoom();
-		if (uidl.hasAttribute("zoom")) {
-			zoom = uidl.getIntAttribute("zoom");
-			if (!uidl.hasAttribute("clat")) {
-				// just set zoom, no center position
-				map.setZoom(zoom);
-			}
-		}
+            for (String string : oldcontrols) {
+                Control control = myControls.get(string);
+                if (control != null) {
+                    map.removeControl(control);
+                }
+                myControls.remove(string);
+            }
 
-		if (uidl.hasAttribute("clat")) {
-			double lat = uidl.getDoubleAttribute("clat");
-			double lon = uidl.getDoubleAttribute("clon");
-			LonLat lonLat = LonLat.create(lon, lat);
-			// expect center point to be in WSG84
-			Projection projection = map.getProjection();
-			lonLat.transform(DEFAULT_PROJECTION, projection);
-			map.setCenter(lonLat, zoom);
-		}
-	}
+        }
+    }
 
-	public void replaceChildComponent(Widget oldComponent, Widget newComponent) {
-		// TODO Auto-generated method stub
+    private void updateZoomAndCenter(UIDL uidl) {
 
-	}
+        if (uidl.hasAttribute("ze_top")) {
+            /*
+             * Zoom to extent
+             */
+            double top = uidl.getDoubleAttribute("ze_top");
+            double right = uidl.getDoubleAttribute("ze_right");
+            double bottom = uidl.getDoubleAttribute("ze_bottom");
+            double left = uidl.getDoubleAttribute("ze_left");
+            Bounds bounds = Bounds.create(left, bottom, right, top);
+            bounds.transform(DEFAULT_PROJECTION, getMap().getProjection());
+            getMap().zoomToExtent(bounds);
+            return;
+        }
 
-	public boolean hasChildComponent(Widget component) {
-		return fakePaintables.getWidgetIndex(component) != -1;
-	}
+        int zoom = map.getZoom();
+        if (uidl.hasAttribute("zoom")) {
+            zoom = uidl.getIntAttribute("zoom");
+            if (!uidl.hasAttribute("clat")) {
+                // just set zoom, no center position
+                map.setZoom(zoom);
+            }
+        }
 
-	public void updateCaption(Paintable component, UIDL uidl) {
-		// TODO Auto-generated method stub
+        if (uidl.hasAttribute("clat")) {
+            double lat = uidl.getDoubleAttribute("clat");
+            double lon = uidl.getDoubleAttribute("clon");
+            LonLat lonLat = LonLat.create(lon, lat);
+            // expect center point to be in WSG84
+            Projection projection = map.getProjection();
+            lonLat.transform(DEFAULT_PROJECTION, projection);
+            map.setCenter(lonLat, zoom);
+        }
+    }
 
-	}
+    public void replaceChildComponent(Widget oldComponent, Widget newComponent) {
+        // TODO Auto-generated method stub
 
-	public boolean requestLayout(Set<Paintable> children) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    }
 
-	public RenderSpace getAllocatedSpace(Widget child) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public boolean hasChildComponent(Widget component) {
+        return fakePaintables.getWidgetIndex(component) != -1;
+    }
 
-	public Map getMap() {
-		return map;
-	}
+    public void updateCaption(Paintable component, UIDL uidl) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public boolean requestLayout(Set<Paintable> children) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public RenderSpace getAllocatedSpace(Widget child) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Map getMap() {
+        return map;
+    }
 }
