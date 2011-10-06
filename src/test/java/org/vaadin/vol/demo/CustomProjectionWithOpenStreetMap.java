f0ad1abfd8a429d168b828c1624b06c7648d57e0
matti.tahvonen <matti.tahvonen@37e336fd-f6aa-ac36-3442-120c8ea9bd2d>
2011-10-06 15:06:04 +0000

    test enhancements
    git-svn-id: http://vopenlayers.googlecode.com/svn/trunk@87 37e336fd-f6aa-ac36-3442-120c8ea9bd2d

--- src/test/java/org/vaadin/vol/demo/CustomProjectionWithOpenStreetMap.java
+++ src/test/java/org/vaadin/vol/demo/CustomProjectionWithOpenStreetMap.java
@@ -6,12 +6,14 @@ import org.vaadin.vol.OpenStreetMapLayer;
 /**
  * Coordinate system is KKJ / Finland Uniform Coordinate System
  * 
- * FIXME "EPSG:2393" points over 9009l3 (OSM) don't appear to fit perfectly
+ * FIXME "EPSG:2393" points over 900913 (OSM) don't appear to fit perfectly, an
+ * issue with proj4js configuration?
  */
 public class CustomProjectionWithOpenStreetMap extends CustomProjectionWithWms {
-    
+
     protected void addBaseLayer(OpenLayersMap openLayersMap) {
         openLayersMap.addLayer(new OpenStreetMapLayer());
+
     }
 
 }
--- src/test/java/org/vaadin/vol/demo/CustomProjectionWithWms.java
+++ src/test/java/org/vaadin/vol/demo/CustomProjectionWithWms.java
@@ -10,9 +10,9 @@ import com.vaadin.ui.Component;
 
 /**
  * Coordinate system is KKJ / Finland Uniform Coordinate System as is base map.
- *
+ * 
  * TODO Is there a free public WMS service we could use for this test?
- *
+ * 
  */
 public class CustomProjectionWithWms extends AbstractVOLTest {
 
@@ -25,39 +25,55 @@ public class CustomProjectionWithWms extends AbstractVOLTest {
         openLayersMap.getControls().add(Control.Navigation);
         openLayersMap.getControls().add(Control.PanZoomBar);
         openLayersMap.getControls().add(Control.MousePosition);
-        
-        addLayer(openLayersMap);
-        
+
+        addBaseLayer(openLayersMap);
+
         // Center on Vaadin HQ
-        openLayersMap.setCenter(3241719.686,6713842.519);
+        openLayersMap.setCenter(3241692.676, 6713854.524);
         openLayersMap.setZoom(8);
-        
+
         // Add vector on Vaadin HQ
         VectorLayer vectorLayer = new VectorLayer();
         // vector layer inherits projection from map
-        // this should default to using base laer coordinates
-        vectorLayer.addComponent(new PointVector(3241719.686,6713842.519));
-        
+        // this should default to using base layer coordinates 2393
+        vectorLayer.addComponent(new PointVector(3241692.676, 6713854.524));
+
+        /*
+         * This works fine from 900913 and 4326 align perfectly with 900913 base
+         * layer, but the third marker should be on same spot (Defined with
+         * 2393, projection tested with kansalaisen.karttapaikka.fi). If a base
+         * layer is in 2393, 4326 and 2393 align ok, 900913 badly. So apparently
+         * direct transforms 900913 <> 2393 are somewhat broken.
+         */
+        PointVector pointVector = new PointVector(2482517.039867359,
+                8501187.741456728);
+        pointVector.setProjection("EPSG:900913");
+        vectorLayer.addComponent(pointVector);
+        pointVector = new PointVector(22.30083, 60.452541);
+        pointVector.setProjection("EPSG:4326");
+        vectorLayer.addComponent(pointVector);
+
         // next building (hospital) in different projection
-        PointVector pointVector = new PointVector(22.2959818,60.45406552);
+        pointVector = new PointVector(22.2959818, 60.45406552);
         // override vectors projection
         pointVector.setProjection("EPSG:4326");
         vectorLayer.addComponent(pointVector);
-        
+
         openLayersMap.addLayer(vectorLayer);
         return openLayersMap;
     }
 
-    protected void addLayer(OpenLayersMap openLayersMap) {
-        openLayersMap.setJsMapOptions("{ maxExtent: new OpenLayers.Bounds(0,0,3750000,7850000), units: 'm', resolutions: [3172.931125, 794.034895, 264.639239, 132.291931, 52.9167724, 26.458386, 13.229193, 6.614597, 4.234905, 2.116577, 1],projection: \"EPSG:2393\"}");
+    protected void addBaseLayer(OpenLayersMap openLayersMap) {
+        openLayersMap
+                .setJsMapOptions("{ maxExtent: new OpenLayers.Bounds(0,0,3750000,7850000), units: 'm', resolutions: [3172.931125, 794.034895, 264.639239, 132.291931, 52.9167724, 26.458386, 13.229193, 6.614597, 4.234905, 2.116577, 1],projection: \"EPSG:2393\"}");
         WebMapServiceLayer webMapServiceLayer = new WebMapServiceLayer();
         webMapServiceLayer.setBaseLayer(true);
-        webMapServiceLayer.setUri("http://www.myserver.fi/myservice");
+        webMapServiceLayer
+                .setUri("http://www.karttakone.fi/wms/ddde5aedeb05b6124a42ac8af7513f62/ykj/logica_ykj");
         webMapServiceLayer.setProjection("EPSG:2393");
-        webMapServiceLayer.setLayers("mylayer");
+        webMapServiceLayer.setLayers("logica_ykj");
         webMapServiceLayer.setFormat("image/png");
         openLayersMap.addLayer(webMapServiceLayer);
     }
-
 
 }
