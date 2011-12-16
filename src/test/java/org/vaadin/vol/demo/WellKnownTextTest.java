3be527879552a8bd5a691ab5fe9157876bb29290
matti.tahvonen <matti.tahvonen@37e336fd-f6aa-ac36-3442-120c8ea9bd2d>
2011-12-16 09:35:12 +0000

    WKT layer test now uses OSM layer as background
    git-svn-id: http://vopenlayers.googlecode.com/svn/trunk@106 37e336fd-f6aa-ac36-3442-120c8ea9bd2d

--- src/test/java/org/vaadin/vol/demo/WellKnownTextTest.java
+++ src/test/java/org/vaadin/vol/demo/WellKnownTextTest.java
@@ -3,6 +3,7 @@ package org.vaadin.vol.demo;
 import org.vaadin.vol.AbstractAutoPopulatedVectorLayer.FeatureSelectedListener;
 import org.vaadin.vol.Bounds;
 import org.vaadin.vol.OpenLayersMap;
+import org.vaadin.vol.OpenStreetMapLayer;
 import org.vaadin.vol.Point;
 import org.vaadin.vol.Style;
 import org.vaadin.vol.StyleMap;
@@ -24,7 +25,8 @@ public class WellKnownTextTest extends AbstractVOLTest {
         webMapServiceLayer.setUri("http://tilecache.osgeo.org/wms-c/Basic.py");
         webMapServiceLayer.setBaseLayer(true);
         webMapServiceLayer.setDisplayName("Base map");
-        openLayersMap.addLayer(webMapServiceLayer);
+//        openLayersMap.addLayer(webMapServiceLayer);
+        openLayersMap.addLayer(new OpenStreetMapLayer());
 
         WellKnownTextLayer wktLayer = new WellKnownTextLayer();
         wktLayer.setWellKnownText("GEOMETRYCOLLECTION(POLYGON((-114.55859375 50.626953125, -113.85546875 30.939453125, -95.046875 32.345703125, -96.62890625 50.275390625, -114.55859375 50.626953125),LINESTRING(-103.30859375 45.705078125, -98.73828125 40.607421875, -102.95703125 36.388671875, -102.60546875 36.388671875),LINESTRING(-111.74609375 47.990234375, -108.23046875 47.990234375, -106.82421875 45.705078125, -106.6484375 43.068359375, -107 41.837890625, -106.296875 41.310546875, -105.41796875 41.310546875, -104.01171875 40.783203125, -106.47265625 40.080078125, -107 39.904296875, -107 37.794921875, -105.76953125 35.861328125, -105.76953125 33.751953125, -110.515625 33.048828125)))");
